d1e3a383dd3156e66ba24799aeaf1871985cbaa2
Matheuscsceil <math.cs.ceil@gmail.com>
2019-11-05 16:57:18 -0300

    Ajustes

--- app/Http/Controllers/ContraChequeController.php
+++ app/Http/Controllers/ContraChequeController.php
@@ -9,9 +9,9 @@ class ContraChequeController extends Controller
 {
     public function index(){
         return DB::table('persona.vw_contracheque_trabalhadores')
-            ->where('nome_trabalhador', 'like', '%SOUSA%')
-            ->orWhere('mes_calculo', '=', '7')
-            ->orWhere('ano_calculo', '=', '2017')
+            ->where('cbo_trabalhador', '=', '521125')
+            /* ->orWhere('mes_calculo', '=', '7')
+            ->orWhere('ano_calculo', '=', '2017') */
             ->paginate(10);
     }
 }
