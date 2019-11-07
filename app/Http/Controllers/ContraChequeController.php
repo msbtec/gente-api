05256f48553c9047ea06ad3119c35b794a16c03f
Matheuscsceil <math.cs.ceil@gmail.com>
2019-11-07 17:23:25 -0300

    Filtro busca

--- app/Http/Controllers/ContraChequeController.php
+++ app/Http/Controllers/ContraChequeController.php
@@ -9,9 +9,10 @@ class ContraChequeController extends Controller
 {
     public function index(){
         return DB::table('persona.vw_contracheque_trabalhadores')
-            ->where('cbo_trabalhador', '=', '521125')
+            ->where('cbo_trabalhador', '=', '411005')
             /* ->orWhere('mes_calculo', '=', '7')
             ->orWhere('ano_calculo', '=', '2017') */
+            ->orderBy('ano_calculo', 'DESC')
             ->paginate(10);
     }
 }
